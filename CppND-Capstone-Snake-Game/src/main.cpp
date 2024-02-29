2ad1606c76a372f0a90891333d348f61b9f5350a
AdrianPayan <134498089+AdrianPayan@users.noreply.github.com>
2024-02-29 10:28:35 +0100

    Add files via upload

---
+++ CppND-Capstone-Snake-Game/src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "controller.h"
+#include "game.h"
+#include "renderer.h"
+
+int main() {
+  constexpr std::size_t kFramesPerSecond{60};
+  constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
+  constexpr std::size_t kScreenWidth{640};
+  constexpr std::size_t kScreenHeight{640};
+  constexpr std::size_t kGridWidth{32};
+  constexpr std::size_t kGridHeight{32};
+  
+  std::string playerName;
+  std::cout << "Enter username: ";
+  std::getline(std::cin, playerName);
+
+  Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
+  Controller controller;
+  {
+    Game game(kGridWidth, kGridHeight);
+    game.Run(controller, renderer, kMsPerFrame);
+    game.SaveScore(playerName);
+    game.DisplayTopPlayers();
+
+    std::cout << "Game has terminated successfully!\n";
+    std::cout << "Score: " << game.GetScore() << "\n";
+    std::cout << "Size: " << game.GetSize() << "\n";
+  }
+  return 0;
+}
